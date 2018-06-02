ff016f6e0f335f17a52e6af73bff1c8809a4d63d
zengjianlu <zengjianlu@360jk.com>
2018-06-02 16:53:44 +0800

    删除target

--- dubbo-service/src/main/java/hystrix/HelloServiceImpl.java
+++ dubbo-service/src/main/java/hystrix/HelloServiceImpl.java
@@ -1,12 +1,19 @@
 package hystrix;
 
 import com.galaxy.hystrix.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service("helloService")
 public class HelloServiceImpl implements HelloService{
+
     @Override
     public String sayHello() {
+        try {
+            Thread.sleep(200l);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return "hello world";
     }
 }
